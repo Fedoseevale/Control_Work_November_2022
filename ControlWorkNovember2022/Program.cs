b595d428e6ebf6b2889285bbc376a0d3e3dc101a
Александр <fedoseevale@gmail.com>
2022-11-12 22:03:15 +0400

    Add Method for defining of required elements of array

--- ControlWorkNovember2022/Program.cs
+++ ControlWorkNovember2022/Program.cs
@@ -1,33 +1,39 @@
-﻿string[] CreateArrayRndInt2(int size)
+﻿string[] CreateArrayString()
 {
     Console.WriteLine("Введите длину массива: ");
-    size = Convert.ToInt32(Console.ReadLine());
+    int size = Convert.ToInt32(Console.ReadLine());
     string[] array = new string[size];
-   
+
     for (int i = 0; i < size; i++)
     {
-        Console.WriteLine($"Введите строку {i+1}: ");
-        array[i] = Console.ReadLine(); 
+        Console.WriteLine($"Введите строку для {i + 1}-го элемента массива: ");
+        array[i] = Console.ReadLine();
     }
     return array;
 }
-
 
 void PrintArray(string[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) Console.Write($"'{array[i]}'', ");
+        if (i < array.Length - 1) Console.Write($"'{array[i]}', ");
         else Console.Write($"'{array[i]}'");
     }
     Console.WriteLine("]");
 }
 
-
-
-
-string[] arr = CreateArrayRndInt2(10);
-PrintArray(arr);
-
+int NumberOfReqElementsOfArrayString(string[] arr)
+{
+    int len = default;
+    string[] array = new string[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            len = len + 1;
+        }
+    }
+    return len;
+}
 
